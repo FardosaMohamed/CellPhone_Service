7c59b337273185f5fea4cccffb0d8fd12f20db4b
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-04-15 17:23:34 -0700

    intial commit

--- src/CellPhone.java
+++ src/CellPhone.java
@@ -1,15 +1,60 @@
 
 public class CellPhone{
     // These are fields (a.k.a instance variables)
-    long serialNumber;
+    long serialNumber ;
     String model;
     String carrier;
-    String phoneNumber;
+    String phoneNumber ;
     String owner;
 
-    public void getInfo(){
 
+    public CellPhone(){
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
     }
 
-    public void
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(long serialNumber){
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
+
--- src/CellPhoneApplication.java
+++ src/CellPhoneApplication.java
@@ -1,5 +1,15 @@
 public class CellPhoneApplication {
     public static void main(String[] args) {
-
+     CellPhone c1 = new CellPhone();
+     c1.serialNumber = 123456;
+     c1.model = "Toyota";
+     c1.phoneNumber = "1209867890";
+     c1.carrier = "T-Mobile";
+     c1.owner = "Fardosa Mohamed";
+        System.out.println( "What is the serial number? " + c1.serialNumber);
+        System.out.println( "What model is the phone? " + c1.model);
+        System.out.println("What is the carrier? " + c1.carrier);
+        System.out.println( "What is the phone number? " + c1.phoneNumber);
+        System.out.println( "Who is the owner of the phone? " + c1.owner);
     }
 }
