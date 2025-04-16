2cb75b405755d114d0b535c754a7ca9ab4b6a735
fardosamohamed <Fardosa.Mohamed2@seattlecolleges.edu>
2025-04-15 17:23:16 -0700

    Initial commit

---
+++ src/CellPhone.java
@@ -0,0 +1,15 @@
+
+public class CellPhone{
+    // These are fields (a.k.a instance variables)
+    long serialNumber;
+    String model;
+    String carrier;
+    String phoneNumber;
+    String owner;
+
+    public void getInfo(){
+
+    }
+
+    public void
+}
